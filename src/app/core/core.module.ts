84568ad5f2a370c7d223a48f6c5740b7b99d4d04
Marcin8013 <marcing2067@wp.pl>
2022-03-26 08:46:53 +0100

    Add nav; create core & shared modules

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  imports: [CommonModule, BrowserAnimationsModule, HttpClientModule],
+})
+export class CoreModule {}
