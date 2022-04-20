d8b667f3eafcf07e72f3ab1fa0ef0922aee8d399
Marcin8013 <marcing2067@wp.pl>
2022-04-20 12:16:30 +0200

    Add maxQuantity and featured params to product model

--- src/app/home/shared/home.service.ts
+++ src/app/home/shared/home.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { delay, of } from 'rxjs';
-import { Product } from 'src/app/shared/products/product.model';
+import { PRODUCTS } from 'src/app/products/shared/products';
 
 @Injectable({
   providedIn: 'root',
@@ -10,36 +10,9 @@ export class HomeService {
   constructor(private httpClient: HttpClient) {}
 
   getFeaturedProducts() {
-    const products: Product[] = [
-      {
-        id: '3',
-        name: 'Telefon',
-        price: 950.0,
-        imageUrl:
-          'https://www.industrialempathy.com/img/remote/ZiClJf-1920w.jpg',
-      },
-      {
-        id: '2',
-        name: 'Szczoteczka do zębów',
-        price: 20.0,
-        imageUrl:
-          'https://www.industrialempathy.com/img/remote/ZiClJf-1920w.jpg',
-      },
-      {
-        id: '3',
-        name: 'Telefon',
-        price: 950.0,
-        imageUrl:
-          'https://www.industrialempathy.com/img/remote/ZiClJf-1920w.jpg',
-      },
-      {
-        id: '2',
-        name: 'Szczoteczka do zębów',
-        price: 20.0,
-        imageUrl:
-          'https://www.industrialempathy.com/img/remote/ZiClJf-1920w.jpg',
-      },
-    ];
+    const products = PRODUCTS.filter((p) => {
+      return p.featured === true;
+    })
     return of(products).pipe(delay(300));
   }
 }
--- src/app/products/shared/products.ts
+++ src/app/products/shared/products.ts
@@ -1,32 +1,44 @@
-export const PRODUCTS = [
+import { Product } from 'src/app/shared/products/product.model';
+
+export const PRODUCTS: Product[] = [
   {
     id: '1',
     name: 'Pralka',
     price: 200.0,
     imageUrl: 'https://www.industrialempathy.com/img/remote/ZiClJf-1920w.jpg',
+    maxQuantity: 210,
+    featured: false,
   },
   {
     id: '2',
     name: 'Suszarka do włosów',
     price: 20.0,
     imageUrl: 'https://www.industrialempathy.com/img/remote/ZiClJf-1920w.jpg',
+    maxQuantity: 70,
+    featured: true,
   },
   {
     id: '3',
     name: 'Telefon',
     price: 950.0,
     imageUrl: 'https://www.industrialempathy.com/img/remote/ZiClJf-1920w.jpg',
+    maxQuantity: 250,
+    featured: true,
   },
   {
     id: '2',
     name: 'Szczoteczka do zębów',
     price: 20.0,
     imageUrl: 'https://www.industrialempathy.com/img/remote/ZiClJf-1920w.jpg',
+    maxQuantity: 80,
+    featured: false,
   },
   {
     id: '1',
     name: 'Pluszowy miś',
     price: 60.0,
     imageUrl: 'https://www.industrialempathy.com/img/remote/ZiClJf-1920w.jpg',
+    maxQuantity: 2500,
+    featured: false,
   },
 ];
